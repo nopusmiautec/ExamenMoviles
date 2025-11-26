abbddc5077432d2792cb77361a455b1e37606f2e
Ricardo Alejandro Andrade Robledo <ricardoandraderobledo@gmail.com>
2025-11-25 18:46:18 -0600

    [ADD] Added the rest of the functionalities for the test API

--- app/src/main/java/com/app/examenapp/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/app/examenapp/presentation/screens/HomeScreen.kt
@@ -1,26 +1,38 @@
 package com.app.examenapp.presentation.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Suppress("ktlint:standard:function-naming")
 @Composable
-fun HomeScreen() {
+fun HomeScreen(onNavigateToCharacters: () -> Unit) {
     Scaffold { paddingValues ->
-        Box(
+        Column(
             modifier =
                 Modifier
                     .fillMaxSize()
                     .padding(paddingValues),
-            contentAlignment = Alignment.Center,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = "Work in progress.")
+            Text(text = "Menú Principal")
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(onClick = { onNavigateToCharacters() }) {
+                Text(text = "Ver Personajes (API)")
+            }
         }
     }
 }
